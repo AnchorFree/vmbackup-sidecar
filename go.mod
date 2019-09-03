b5ecf0feb6ba14d3312e34e1576f21191cb85432
Pavel Dmytrenko <p.dmytrenko@anchorfree.com>
2019-09-03 18:35:19 +0300

    Finalize tests for pkg/env

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/AnchorFree/vmbackup-sidecar
 
 go 1.12
 
-require github.com/google/shlex v0.0.0-20181106134648-c34317bd91bf
+require (
+	github.com/google/shlex v0.0.0-20181106134648-c34317bd91bf
+	github.com/stretchr/testify v1.4.0
+)
--- pkg/env/env_test.go
+++ pkg/env/env_test.go
@@ -1,57 +1,98 @@
 package env
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type envTestCase struct {
 	descr       string
 	host        string
-	port        uint16
+	port        string
 	bucketName  string
 	dataPath    string
-	expectedErr error
+	errExpected bool
 }
 
 var testCases = []envTestCase{
 	{
-		descr:       "All env vars are set",
+		descr:       "All vars are set; no error",
 		host:        "localhost",
-		port:        4242,
+		port:        "4242",
 		bucketName:  "foo-bucket",
 		dataPath:    "/foo/data",
-		expectedErr: nil,
+		errExpected: false,
+	},
+	{
+		descr:       fmt.Sprintf("%s is not set; error", HostVarName),
+		host:        "",
+		port:        "4242",
+		bucketName:  "foo-bucket",
+		dataPath:    "/foo/data",
+		errExpected: true,
+	},
+	{
+		descr:       fmt.Sprintf("%s is not set; error", PortVarName),
+		host:        "localhost",
+		port:        "",
+		bucketName:  "foo-bucket",
+		dataPath:    "/foo/data",
+		errExpected: true,
+	},
+	{
+		descr:       fmt.Sprintf("%s is not set; error", BucketVarName),
+		host:        "localhost",
+		port:        "4242",
+		bucketName:  "",
+		dataPath:    "/foo/data",
+		errExpected: true,
+	},
+	{
+		descr:       fmt.Sprintf("%s is not set; error", DataPathVarName),
+		host:        "localhost",
+		port:        "4242",
+		bucketName:  "foo-bucket",
+		dataPath:    "",
+		errExpected: true,
 	},
 }
 
 func (s envTestCase) getExpectedCfg() *Config {
-	return &Config{
-		Host:       s.host,
-		Port:       s.port,
-		BucketName: s.bucketName,
-		DataPath:   s.dataPath,
+	var c Config
+	if !s.errExpected {
+		p, err := strconv.ParseUint(s.port, 10, 16)
+		if err != nil {
+			panic(err)
+		}
+		c = Config{
+			Host:       s.host,
+			Port:       uint16(p),
+			BucketName: s.bucketName,
+			DataPath:   s.dataPath,
+		}
 	}
+	return &c
 }
 
 func TestGetConfig(t *testing.T) {
 	for _, tCase := range testCases {
 		// Env setup
 		os.Setenv(HostVarName, tCase.host)
-		os.Setenv(PortVarName, strconv.Itoa(int(tCase.port)))
+		os.Setenv(PortVarName, tCase.port)
 		os.Setenv(BucketVarName, tCase.bucketName)
 		os.Setenv(DataPathVarName, tCase.dataPath)
 
 		// Actual test
 		res, err := GetConfig()
-		expectedCfg := tCase.getExpectedCfg()
-		if *res != *expectedCfg {
-			t.Fatalf("FAIL: %s\nExpected: %+v\nActual: %+v", tCase.descr, expectedCfg, res)
+		assert.Equal(t, tCase.getExpectedCfg(), res, tCase.descr)
+		if tCase.errExpected {
+			assert.NotNil(t, err, tCase.descr)
+		} else {
+			assert.Nil(t, err, tCase.descr)
 		}
-		if err != tCase.expectedErr {
-			t.Fatalf("FAIL: %s\nExpected: '%s'\nActual: '%s'", tCase.descr, tCase.expectedErr, err)
-		}
-		t.Logf("PASS: %s", tCase.descr)
 	}
 }
